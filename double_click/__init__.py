0f327b901b19e4ae3aa31ed76d126e3f1c8c7f3f
wbeasley <wbeasley@cylance.com>
2020-02-24 12:45:09 -0600

    Moved echo to a utils file. Created functions for display_version, update_package, and ensure_latest_package.

--- double_click/__init__.py
+++ double_click/__init__.py
@@ -1,2 +1,3 @@
-from double_click.echo import echo
-from double_click.user import ActiveUser
+from double_click.utils import display_version, echo, update_package, ensure_latest_package
+from double_click.user import User, UserSession
+
