a64c7e212da763a9f18335f3801b22ac1ff17f3f
wbeasley <wbeasley@cylance.com>
2020-02-28 11:58:13 -0600

    Updated imports in __init__

--- double_click/__init__.py
+++ double_click/__init__.py
@@ -1,4 +1,5 @@
 from double_click.utils import display_version, echo, update_package, ensure_latest_package
-from double_click.user import User
 from double_click.request import GeneralSession, UserSession
+from double_click.models import Model
+from double_click.user import User
 
