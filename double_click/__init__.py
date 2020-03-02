b5a2251d55ef68116ecf04c96cf9e10797df9561
wbeasley <wbeasley@cylance.com>
2020-03-02 14:15:13 -0600

    New ModelAuth class. Refactor of models.Model, new methods _cache_retrieve and _cache_set. Using protected attributes instead of property for model.Model

--- double_click/__init__.py
+++ double_click/__init__.py
@@ -1,5 +1,5 @@
 from double_click.utils import display_version, echo, update_package, ensure_latest_package
 from double_click.request import GeneralSession, UserSession
-from double_click.models import Model
+from double_click.models import Model, ModelAuth
 from double_click.user import User
 
