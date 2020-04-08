5369178ebde621f2779a9282ee6dd51f8d803ddb
wbeasley <wbeasley@cylance.com>
2020-04-08 10:19:19 -0500

    Set a default terminal size if running in supervisor.

--- double_click/__init__.py
+++ double_click/__init__.py
@@ -1,8 +1,13 @@
 import os
 
-width, height = os.get_terminal_size()
-os.environ.setdefault('width', str(width))
-os.environ.setdefault('LINES', str(height))
+try:
+    width, height = os.get_terminal_size()
+    os.environ.setdefault('width', str(width))
+    os.environ.setdefault('LINES', str(height))
+except OSError:
+    os.environ.setdefault('width', "0")
+    os.environ.setdefault('LINES', "0")
+
 
 from double_click.utils import display_version, echo, update_package, ensure_latest_package
 from double_click.request import GeneralSession, UserSession
